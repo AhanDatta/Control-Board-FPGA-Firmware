12b29be5c65c5ab4a07a2f050537383a2060f769
AhanDatta <ahan.datta312@gmail.com>
2024-12-17 15:45:55 -0500

    Rewrite: Turn SPI_driver into a state machine
    Read and write processes both work. Need to fix the wrapper and create the testbench.

--- shared/ip_repo/SPI_driver/SPI_driver.sv
+++ shared/ip_repo/SPI_driver/SPI_driver.sv
@@ -1,15 +1,4 @@
-//Take in cmnd from IPIF (AXI) interface
-/*
-Create IPIF struct with all values to be written
-*/
-//Update state machine
-/*
-detect new IPIF cmnd, by setting one of the bits as a self-reseting flag
-Do cmnd
-*/
-//Send out command
-
-module SPI_driver #(
+module spi_driver #(
     parameter REG_WIDTH = 8,
     parameter MSG_LEN = 2
 )(
@@ -21,63 +10,101 @@ module SPI_driver #(
     input logic [REG_WIDTH*(MSG_LEN-1)-1:0] write_data,
     output logic [REG_WIDTH*(MSG_LEN-1)-1:0] data_read_from_reg,
     output logic serial_out,
-    output logic spi_clk
+    output logic spi_clk,
+    output logic transaction_complete
 );
-    //working address and write data
-    logic [REG_WIDTH*(MSG_LEN-1) - 1:0] data_to_write;
-    logic [REG_WIDTH*(MSG_LEN-1) - 1:0] serial_in_deserializer;
+
+    typedef enum logic [1:0] {
+        IDLE,
+        SEND_ADDRESS,
+        TRANSFER_DATA,
+        COMPLETE
+    } spi_state_t;
+
+    //internal signals
+    spi_state_t current_state;
+    logic prev_new_command;
+    logic spi_clk_en;
+    logic [7:0] bit_counter;
     logic [REG_WIDTH-1:0] addr;
-    logic [7:0] clk_counter;
-    logic spi_clk_enable;
+    logic [REG_WIDTH*(MSG_LEN-1)-1:0] data_to_write;
+    logic [REG_WIDTH*(MSG_LEN-1)-1:0] serial_in_buffer;
 
-    //on new instruction, load data
-    always_ff @(posedge new_command or negedge rstn) begin
-        if (!rstn) begin
-            data_to_write <= '0;
-            addr <= '0;
-            clk_counter <= REG_WIDTH*MSG_LEN + 1;
-        end
-        else begin
-            data_to_write <= write_data;
-            addr <= register_addr;
-            clk_counter <= '0;
-        end
-    end
-
-    //Starts the clock when we want to send a message
+    //clock generation
     always_comb begin
-        if (spi_clk_enable) begin
+        if (!rstn) begin
+            spi_clk = 1'b0;
+        end
+        else if (spi_clk_en) begin
             spi_clk = ~clk;
         end
-        else  begin
-            spi_clk = 0;
+        else begin
+            spi_clk = 1'b0;
         end
     end
 
+    //main state machine
     always_ff @(posedge clk or negedge rstn) begin
         if (!rstn) begin
-            spi_clk_enable <= 0;
-            serial_out <= 0;
-        end
-        else if (clk_counter < REG_WIDTH) begin //sending the address
-            spi_clk_enable <= 1;
-            serial_out <= addr[clk_counter];
-            clk_counter <= clk_counter + 1;
-        end
-        else if (clk_counter < REG_WIDTH*MSG_LEN) begin
-            spi_clk_enable <= 1;
-            serial_out <= data_to_write[clk_counter-REG_WIDTH];
-            serial_in_deserializer[clk_counter-REG_WIDTH] <= serial_in; 
-            clk_counter <= clk_counter + 1;
-        end
-        else if (clk_counter == REG_WIDTH*MSG_LEN + 1) begin
-            spi_clk_enable <= 0;
-            serial_out <= 0;
+            current_state <= IDLE;
+            serial_out <= 1'b0;
+            data_read_from_reg <= '0;
+            serial_in_buffer <= '0;
+            bit_counter <= '0;
+            transaction_complete <= 1'b0;
+            prev_new_command <= 1'b0;
+            spi_clk_en <= 1'b0;
         end
         else begin
-            spi_clk_enable <= 0;
-            serial_out <= 0;
+            case (current_state)
+
+                IDLE: begin
+                    prev_new_command <= new_command;
+                    bit_counter <= '0;
+                    transaction_complete <= 1'b0;
+                    spi_clk_en <= 1'b0;
+                    if (new_command && ~prev_new_command) begin
+                        //capture input parameters
+                        addr <= register_addr;
+                        data_to_write <= write_data;
+                        current_state <= SEND_ADDRESS;
+                    end
+                end
+
+                SEND_ADDRESS: begin
+                    //shift out address bits
+                    if (bit_counter < (REG_WIDTH-1)) begin
+                        serial_out <= addr[REG_WIDTH - 1 - bit_counter];
+                        bit_counter <= bit_counter + 1;
+                        spi_clk_en <= 1'b1;
+                    end
+                    else begin //transitions and sends the last bit at the same time
+                        serial_out <= addr[0];
+                        bit_counter <= '0;
+                        current_state <= TRANSFER_DATA;
+                    end
+                end
+
+                TRANSFER_DATA: begin
+                    if (bit_counter < REG_WIDTH * (MSG_LEN-1)) begin
+                        serial_out <= data_to_write[(MSG_LEN-1)*REG_WIDTH - 1 - bit_counter];
+                        serial_in_buffer <= {serial_in, serial_in_buffer[REG_WIDTH * (MSG_LEN-1) - 1 : 1]};
+                        bit_counter <= bit_counter + 1;
+                    end
+                    else begin
+                        serial_in_buffer <= {serial_in, serial_in_buffer[REG_WIDTH * (MSG_LEN-1) - 1 : 1]}; //catches the last bit
+                        spi_clk_en <= 1'b0;
+                        current_state <= COMPLETE;
+                    end
+                end
+
+                COMPLETE: begin
+                    data_read_from_reg <= serial_in_buffer;
+                    transaction_complete <= 1'b1;
+                    serial_out <= 1'b0;
+                    current_state <= IDLE;
+                end
+            endcase
         end
     end
-
 endmodule

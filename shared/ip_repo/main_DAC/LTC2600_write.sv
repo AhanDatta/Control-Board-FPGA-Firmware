5224c67cbba018ae8f102006a17470512d4cda06
AhanDatta <ahan.datta312@gmail.com>
2025-02-05 14:02:43 -0600

    Finishing LTC2600_write

--- shared/ip_repo/main_DAC/LTC2600_write.sv
+++ shared/ip_repo/main_DAC/LTC2600_write.sv
@@ -11,115 +11,27 @@ IPIF Command to Command LUT:
 */
 
 module LTC2600_write #(
-    parameter integer DATA_WIDTH = 16,
-    parameter integer C_S_AXI_DATA_WIDTH = 32,
-    parameter integer C_S_AXI_ADDR_WIDTH = 32,
-    parameter integer N_REG = 4
+    parameter integer DATA_WIDTH = 16
 ) (
     input logic rstn,
     input logic clk, //50 MHz
 
+    //instruction inputs
+    input logic send_new_cmd,
+    input logic [3:0] command,
+    input logic [3:0] address,
+    input logic [DATA_WIDTH-1:0] data,
+
     //DAC wires
-    input logic sdo;
-    output logic sck;
-    output logic sdi;
-    output logic csb;
+    input logic sdo,
+    output logic sck,
+    output logic sdi,
+    output logic csb,
+    output logic clrb, //DAC reset
 
     //outputs
-    output logic write_complete;
-
-    //IPIF interface
-    //configuration parameter interface 
-    input logic                                  IPIF_Bus2IP_resetn,
-    input logic [(C_S_AXI_ADDR_WIDTH-1) : 0]     IPIF_Bus2IP_Addr, //unused
-    input logic                                  IPIF_Bus2IP_RNW, //unused
-    input logic [((C_S_AXI_DATA_WIDTH/8)-1) : 0] IPIF_Bus2IP_BE, //unused
-    input logic [0 : 0]                          IPIF_Bus2IP_CS, //unused
-    input logic [N_REG-1 : 0]                    IPIF_Bus2IP_RdCE, 
-    input logic [N_REG-1 : 0]                    IPIF_Bus2IP_WrCE,
-    input logic [(C_S_AXI_DATA_WIDTH-1) : 0]     IPIF_Bus2IP_Data,
-    output logic [(C_S_AXI_DATA_WIDTH-1) : 0]    IPIF_IP2Bus_Data,
-    output logic                                 IPIF_IP2Bus_WrAck,
-    output logic                                 IPIF_IP2Bus_RdAck,
-    output logic                                 IPIF_IP2Bus_Error,
+    output logic write_complete
 );
-
-    typedef struct       packed{ 
-      // Register 3
-      logic [30:0]      padding3;
-      logic             ready_signal;
-      // Register 2
-      logic [31:0]      padding2;
-      // Register 1
-      logic [31:0]      padding1;
-      // Register 0
-      logic [7:0]      padding0;
-      logic [3:0]       command;
-      logic [3:0]       address;
-      logic[DATA_WIDTH-1:0] data;
-   } param_t;
-
-    param_t params_from_IP; //use this
-    param_t params_from_bus;
-    param_t params_to_IP; //use this
-    param_t params_to_bus;
-
-    always_comb begin
-      params_from_IP = params_to_IP;
-      //More efficient to explicitely zero padding 
-      params_from_IP.padding0   = '0;
-      params_from_IP.padding1   = '0;
-      params_from_IP.padding2   = '0;
-      params_from_IP.padding3   = '0;
-   end
-
-    IPIF_parameterDecode
-   #(
-     .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
-     .N_REG(N_REG),
-     .PARAM_T(param_t),
-     .DEFAULTS({32'h0, 32'd1, 32'h0, 32'b0}),
-     .SELF_RESET(128'b1)
-     ) parameterDecoder 
-   (
-    .clk(IPIF_clk),
-    
-    .IPIF_bus2ip_data(IPIF_Bus2IP_Data),  
-    .IPIF_bus2ip_rdce(IPIF_Bus2IP_RdCE),
-    .IPIF_bus2ip_resetn(IPIF_Bus2IP_resetn),
-    .IPIF_bus2ip_wrce(IPIF_Bus2IP_WrCE),
-    .IPIF_ip2bus_data(IPIF_IP2Bus_Data),
-    .IPIF_ip2bus_rdack(IPIF_IP2Bus_RdAck),
-    .IPIF_ip2bus_wrack(IPIF_IP2Bus_WrAck),
-    
-    .parameters_out(params_from_bus),
-    .parameters_in(params_to_bus)
-    );
-
-   IPIF_clock_converter 
-    #(
-     .INCLUDE_SYNCHRONIZER(1),
-     .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
-     .N_REG(N_REG),
-     .PARAM_T(param_t)
-     ) IPIF_clock_conv 
-    (
-    .IP_clk(clk),
-    .bus_clk(IPIF_clk),
-    .params_from_IP(params_from_IP),
-    .params_from_bus(params_from_bus),
-    .params_to_IP(params_to_IP),
-    .params_to_bus(params_to_bus)
-    );
-
-    typedef enum logic [3:0] {
-        WRITE_TO_REG_N = 4'b0,
-        POWER_UP_REG_N = 4'b1,
-        WRITE_TO_N_POWER_ALL = 4'd2,
-        WRITE_TO_N_POWER_N = 4'd3,
-        POWER_DOWN_N = 4'd4,
-        NO_OPERATION = 4'b1111
-    } command_t;
 
     typedef enum logic [2:0] {
         IDLE,
@@ -129,41 +41,36 @@ module LTC2600_write #(
         COMPLETE
     } state_t;
 
-    command_t command;
     state_t state;
-    logic [3:0] address;
-    logic [DATA_WIDTH-1:0] data;
-    logic prev_ready_signal;
+    logic prev_send_new_cmd;
     logic [8:0] clk_counter;
 
     always_comb begin
         if (!rstn) begin
             sck = 1'b0;
-            command = command_t'(params_to_IP.command);
-            address = params_to_IP.address;
-            data = params_to_IP.data;
         end
         else begin
             sck = clk; //write clock always on, since we have csb
         end
     end
 
+    assign clrb = rstn; //chip resets at the same time as of the FPGA
+
     always_ff @(posedge clk or negedge rstn) begin
         if (!rstn) begin
             state <= IDLE;
-            prev_ready_signal <= 1'b0;
+            prev_send_new_cmd <= 1'b0;
             write_complete <= 1'b0;
             sdi <= 1'b0;
             csb <= 1'b1;
             clk_counter <= 'b0;
         end
         else begin
-            prev_ready_signal <= ready_signal;
+            prev_send_new_cmd <= send_new_cmd;
             case (state) 
-
                 IDLE: begin
                     write_complete <= 1'b0;
-                    if (ready_signal & ~prev_ready_signal) begin
+                    if (send_new_cmd & ~prev_send_new_cmd) begin
                         state <= SEND_COMMAND;
                         sdi <= command[3];
                         csb <= 1'b0;
@@ -196,12 +103,12 @@ module LTC2600_write #(
                     write_complete <= 1'b0;
                     csb <= 1'b0;
                     if (clk_counter == 'd4) begin
-                        state <= SEND_ADDRESS;
+                        state <= SEND_DATA;
                         sdi <= data[DATA_WIDTH - 1];
                         clk_counter <= 'b1;
                     end
                     else begin
-                        state <= SEND_COMMAND;
+                        state <= SEND_ADDRESS;
                         sdi <= address[3 - clk_counter];
                         clk_counter <= clk_counter + 1;
                     end
@@ -209,14 +116,14 @@ module LTC2600_write #(
 
                 SEND_DATA: begin
                     write_complete <= 1'b0;
-                    if (clk_counter == 'd4) begin
+                    if (clk_counter == DATA_WIDTH) begin
                         state <= COMPLETE;
                         sdi <= 1'b0;
                         clk_counter <= 'b1;
                         csb <= 1'b1;
                     end
                     else begin
-                        state <= SEND_COMMAND;
+                        state <= SEND_DATA;
                         sdi <= data[DATA_WIDTH - 1 -clk_counter];
                         clk_counter <= clk_counter + 1;
                         csb <= 1'b0;
@@ -230,7 +137,6 @@ module LTC2600_write #(
                     clk_counter <= 'b0;
                     csb <= 1'b1;
                 end
-
             endcase
         end 
     end
---
+++ shared/ip_repo/main_DAC/LTC2600_write_wrapper.sv
@@ -0,0 +1,123 @@
+module LTC2600_write_wrapper #(
+    parameter integer DATA_WIDTH = 16,
+    parameter integer C_S_AXI_DATA_WIDTH = 32,
+    parameter integer C_S_AXI_ADDR_WIDTH = 32,
+    parameter integer N_REG = 4
+) (
+    input logic rstn,
+    input logic clk, //50 MHz
+
+    //DAC wires
+    input logic sdo,
+    output logic sck,
+    output logic sdi,
+    output logic csb,
+    output logic clrb,
+
+    //outputs
+    output logic write_complete,
+
+    //IPIF interface
+    //configuration parameter interface 
+    input logic                                  IPIF_Bus2IP_resetn,
+    input logic [(C_S_AXI_ADDR_WIDTH-1) : 0]     IPIF_Bus2IP_Addr, //unused
+    input logic                                  IPIF_Bus2IP_RNW, //unused
+    input logic [((C_S_AXI_DATA_WIDTH/8)-1) : 0] IPIF_Bus2IP_BE, //unused
+    input logic [0 : 0]                          IPIF_Bus2IP_CS, //unused
+    input logic [N_REG-1 : 0]                    IPIF_Bus2IP_RdCE, 
+    input logic [N_REG-1 : 0]                    IPIF_Bus2IP_WrCE,
+    input logic [(C_S_AXI_DATA_WIDTH-1) : 0]     IPIF_Bus2IP_Data,
+    output logic [(C_S_AXI_DATA_WIDTH-1) : 0]    IPIF_IP2Bus_Data,
+    output logic                                 IPIF_IP2Bus_WrAck,
+    output logic                                 IPIF_IP2Bus_RdAck,
+    output logic                                 IPIF_IP2Bus_Error
+);
+
+    typedef struct       packed{ 
+      // Register 3
+      logic [30:0]      padding3;
+      logic             send_new_cmd;
+      // Register 2
+      logic [31:0]      padding2;
+      // Register 1
+      logic [31:0]      padding1;
+      // Register 0
+      logic [7:0]      padding0;
+      logic [3:0]       command;
+      logic [3:0]       address;
+      logic[DATA_WIDTH-1:0] data;
+   } param_t;
+
+    param_t params_from_IP; //use this
+    param_t params_from_bus;
+    param_t params_to_IP; //use this
+    param_t params_to_bus;
+
+    always_comb begin
+      params_from_IP = params_to_IP;
+      //More efficient to explicitely zero padding 
+      params_from_IP.padding0   = '0;
+      params_from_IP.padding1   = '0;
+      params_from_IP.padding2   = '0;
+      params_from_IP.padding3   = '0;
+   end
+
+    IPIF_parameterDecode
+   #(
+     .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
+     .N_REG(N_REG),
+     .PARAM_T(param_t),
+     .DEFAULTS({32'h0, 32'd1, 32'h0, 32'b0}),
+     .SELF_RESET(128'b1)
+     ) parameterDecoder 
+   (
+    .clk(IPIF_clk),
+    
+    .IPIF_bus2ip_data(IPIF_Bus2IP_Data),  
+    .IPIF_bus2ip_rdce(IPIF_Bus2IP_RdCE),
+    .IPIF_bus2ip_resetn(IPIF_Bus2IP_resetn),
+    .IPIF_bus2ip_wrce(IPIF_Bus2IP_WrCE),
+    .IPIF_ip2bus_data(IPIF_IP2Bus_Data),
+    .IPIF_ip2bus_rdack(IPIF_IP2Bus_RdAck),
+    .IPIF_ip2bus_wrack(IPIF_IP2Bus_WrAck),
+    
+    .parameters_out(params_from_bus),
+    .parameters_in(params_to_bus)
+    );
+
+   IPIF_clock_converter 
+    #(
+     .INCLUDE_SYNCHRONIZER(1),
+     .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
+     .N_REG(N_REG),
+     .PARAM_T(param_t)
+     ) IPIF_clock_conv 
+    (
+    .IP_clk(clk),
+    .bus_clk(IPIF_clk),
+    .params_from_IP(params_from_IP),
+    .params_from_bus(params_from_bus),
+    .params_to_IP(params_to_IP),
+    .params_to_bus(params_to_bus)
+    );
+
+    LTC2600_write LTC2600_write_inst (
+        .rstn (rstn),
+        .clk (clk),
+        .write_complete (write_complete),
+
+        //instruction inputs
+        .send_new_cmd (params_to_IP.send_new_cmd),
+        .command (params_to_IP.command),
+        .address (params_to_IP.address),
+        .data (params_to_IP.data),
+
+        //DAC wires
+        .sdo (sdo),
+        .sck (sck),
+        .sdi (sdi),
+        .csb (csb),
+        .clrb (clrb)
+    );
+
+endmodule

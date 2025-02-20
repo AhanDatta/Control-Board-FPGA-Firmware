51b6524cd90e7b13fdf81c5cf39bbeceef125ff2
AhanDatta <ahan.datta312@gmail.com>
2025-02-20 14:56:50 -0600

    Adding Mux to ADC FIFO readout

--- shared/ip_repo/main_ADC/AD9228_read.sv
+++ shared/ip_repo/main_ADC/AD9228_read.sv
@@ -1,7 +1,7 @@
 //adc clk (diff -> single ended), csb (single ended) needs to be handled here
 
 module AD9228_read #(
-    parameter NUM_CHANNELS = 8,
+    parameter integer NUM_CHANNELS = 4,
     parameter integer C_S_AXI_DATA_WIDTH = 32,
     parameter integer C_S_AXI_ADDR_WIDTH = 32,
     parameter integer N_REG = 4,
@@ -22,14 +22,14 @@ module AD9228_read #(
     //Common/SPI adc connections
     output logic clk_p,
     output logic clk_n,
-    output logic csb,
 
     //FIFO connections (indexed by channel)
+    input logic [$clog2(NUM_CHANNELS)-1:0] fifo_addr,
     input logic [NUM_CHANNELS-1 : 0] fifo_rd_en,
     input logic [NUM_CHANNELS-1 : 0] fifo_rd_clk,
     output logic [NUM_CHANNELS-1 : 0] fifo_not_empty,
     output logic [NUM_CHANNELS-1 : 0] fifo_full,
-    output logic [NUM_CHANNELS-1 : 0][DATA_WIDTH-1:0] fifo_dout,
+    output logic [DATA_WIDTH-1:0] fifo_dout,
 
     //IPIF interface
     //configuration parameter interface 
@@ -102,17 +102,18 @@ module AD9228_read #(
      .C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
      .N_REG(N_REG),
      .PARAM_T(param_t)
-     ) IPIF_clock_conv 
+    ) IPIF_clock_conv 
     (
-    .IP_clk(clk),
-    .bus_clk(IPIF_clk),
-    .params_from_IP(params_from_IP),
-    .params_from_bus(params_from_bus),
-    .params_to_IP(params_to_IP),
-    .params_to_bus(params_to_bus)
+        .IP_clk(clk),
+        .bus_clk(IPIF_clk),
+        .params_from_IP(params_from_IP),
+        .params_from_bus(params_from_bus),
+        .params_to_IP(params_to_IP),
+        .params_to_bus(params_to_bus)
     );
 
     logic sample_clk;
+    logic [NUM_CHANNELS-1:0][DATA_WIDTH-1:0] premux_fifo_dout;
 
     //generating each of the channels
     genvar i;
@@ -133,10 +134,20 @@ module AD9228_read #(
                 .fifo_rd_clk (fifo_rd_clk[i]),
                 .fifo_not_empty (fifo_not_empty[i]),
                 .fifo_full (fifo_full[i]),
-                .fifo_dout (fifo_dout[i])
+                .fifo_dout (premux_fifo_dout[i])
             );
         end
     endgenerate
+
+    //Muxing the fifo dout
+    always_comb begin
+        if (!rstn) begin
+            fifo_dout <= '0;
+        end
+        else begin
+            fifo_dout <= premux_fifo_dout[fifo_addr];
+        end
+    end
 
     //gating the sampling clock on the IPIF command
     always_comb begin
@@ -158,7 +169,4 @@ module AD9228_read #(
         .diff_p (clk_p),
         .diff_n (clk_n)
     );
-
-    //placeholder for real SPI block
-    assign csb = 1'b1;
 endmodule
